e8c8585513f1e01d68831b59ab1b2ec4377dcb0a
Matt Z <mzelmanowicz2012@fau.edu>
2018-07-07 16:59:42 -0700

    Fixes to location class
    Applied changes to match the database.
    Added

--- src/domain/Location.java
+++ src/domain/Location.java
@@ -1,84 +1,109 @@
 package domain;
 
 public class Location {
-	
-	String locationId;
-	String street;
-	String city;
-	String country;
-	String state;
-	String zip;
-	
-	public Location() {
-		super();
-	}
-	
-	public Location(String locationId, String street, String city, String country, String state, String zip) {
-		super();
-		this.locationId = locationId;
-		this.street = street;
-		this.city = city;
-		this.country = country;
-		this.state = state;
-		this.zip = zip;
-	}
 
-	public String getLocationId() {
-		return locationId;
-	}
+    //This class refers to locations of stores and users..
+    //It is set by admins.
+    
+    String locationId;  //Primary Key. Used as a Foreign Key in the Store class.
+                        //locationId is called in the storeDetailsScreen() function in Tiger.
 
-	public void setLocationId(String locationId) {
-		this.locationId = locationId;
-	}
+    String userID;      //This was missing from original file, but was in database.
+                        //It is a foreign key, referencing userIDs.
+                        //If a location refers to a store, userID should be NULL.
 
-	public String getStreet() {
-		return street;
-	}
+    Float taxrate;      //This was missing from original file, but was in database.
 
-	public void setStreet(String street) {
-		this.street = street;
-	}
+    String street;      //The rest of the fields are never really used/displayed outside LocationService.java.
+                        //However, there is a section in tiger where a customer enters street/city/etc.,
+                        //but it is commented out.
+    String city;
+    String country;
+    String state;
+    String zip;
 
-	public String getCity() {
-		return city;
-	}
+    public Location() {
+        super();
+    }
 
-	public void setCity(String city) {
-		this.city = city;
-	}
+    // The rest of this file is constructer, getters, setters, and toString.
+    public Location(String locationId, String userID, Float taxrate, String street, String city, String country, String state, String zip) {
+        this.locationId = locationId;
+        this.userID = userID;
+        this.taxrate = taxrate;
+        this.street = street;
+        this.city = city;
+        this.country = country;
+        this.state = state;
+        this.zip = zip;
+    }
 
-	public String getCountry() {
-		return country;
-	}
+    public String getLocationId() {
+        return locationId;
+    }
 
-	public void setCountry(String country) {
-		this.country = country;
-	}
+    public void setLocationId(String locationId) {
+        this.locationId = locationId;
+    }
 
-	public String getState() {
-		return state;
-	}
+    public String getUserID() {
+        return userID;
+    }
 
-	public void setState(String state) {
-		this.state = state;
-	}
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
 
-	public String getZip() {
-		return zip;
-	}
+    public Float getTaxrate() {
+        return taxrate;
+    }
 
-	public void setZip(String zip) {
-		this.zip = zip;
-	}
+    public void setTaxrate(Float taxrate) {
+        this.taxrate = taxrate;
+    }
 
-	@Override
-	public String toString() {
-		return "Locations [locationId=" + locationId + ", street=" + street + ", city=" + city + ", country="
-				+ country + ", state=" + state + ", zip=" + zip + "]";
-	}
-	
-	
-	
-	
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "locationId=" + locationId + ", userID=" + userID + ", taxrate=" + taxrate + ", street=" + street + ", city=" + city + ", country=" + country + ", state=" + state + ", zip=" + zip + '}';
+    }
 }
-
